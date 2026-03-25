c4dcb68d19b87971259e50d8228145928ce3659b
Anderson Almeida <andersonpa10pa@gmail.com>
2026-03-25 20:42:38 -0300

    Ajustes nas funcionalidades.

--- src/routes/refeicoes.ts
+++ src/routes/refeicoes.ts
@@ -4,6 +4,25 @@ import { knex } from '../database'
 import { randomUUID } from 'crypto'
 
 export async function refeicoesRoutes(app: FastifyInstance) {
+  app.get('/', async (request, reply) => {
+    const sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      return reply.status(401).send('Acesso não autorizado, inicie a sessão.')
+    }
+
+    const userId = await knex('users')
+      .select('id')
+      .where('session_id', sessionId)
+      .first()
+
+    const refeicoes = await knex('refeicoes')
+      .where('user_id', userId.id)
+      .select()
+
+    return { refeicoes }
+  })
+
   app.post('/', async (request, reply) => {
     const createRefeicaoSchema = z.object({
       nome: z.string().min(3),
@@ -37,8 +56,13 @@ export async function refeicoesRoutes(app: FastifyInstance) {
     return reply.status(201).send()
   })
 
-  app.put('/', async (request, reply) => {
+  app.put('/:id', async (request, reply) => {
+    const getRefeicaoParamsSchema = z.object({
+      id: z.uuid(),
+    })
+
     const sessionId = request.cookies.sessionId
+    const { id } = getRefeicaoParamsSchema.parse(request.params)
 
     if (!sessionId) {
       return reply.status(401).send('Acesso não autorizado, inicie a sessão.')
@@ -56,7 +80,7 @@ export async function refeicoesRoutes(app: FastifyInstance) {
 
     const userId = await knex('users')
       .select('id')
-      .where('session_id', sessionId)
+      .where('session_id', sessionId, id)
       .first()
 
     await knex('refeicoes').where('user_id', userId.id).update({
@@ -68,39 +92,20 @@ export async function refeicoesRoutes(app: FastifyInstance) {
     return reply.status(204).send()
   })
 
-  app.delete('/', async (request, reply) => {
+  app.delete('/:id', async (request, reply) => {
+    const getRefeicaoParamsSchema = z.object({
+      id: z.uuid(),
+    })
+
     const sessionId = request.cookies.sessionId
+    const { id } = getRefeicaoParamsSchema.parse(request.params)
 
     if (!sessionId) {
       return reply.status(401).send('Acesso não autorizado, inicie a sessão.')
     }
 
-    const userId = await knex('users')
-      .select('id')
-      .where('session_id', sessionId)
-      .first()
-
-    await knex('refeicoes').where('user_id', userId).delete()
+    await knex('refeicoes').where('id', id).delete()
 
     return reply.status(200).send()
-  })
-
-  app.get('/', async (request, reply) => {
-    const sessionId = request.cookies.sessionId
-
-    if (!sessionId) {
-      return reply.status(401).send('Acesso não autorizado, inicie a sessão.')
-    }
-
-    const userId = await knex('users')
-      .select('id')
-      .where('session_id', sessionId)
-      .first()
-
-    const refeicoes = await knex('refeicoes')
-      .where('user_id', userId.id)
-      .select()
-
-    return { refeicoes }
   })
 }
